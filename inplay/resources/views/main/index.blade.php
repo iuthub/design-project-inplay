19cdd8d46f0e562b89b81a86ed5e963aff3b5037
Shakhboz Khalikov <sh.khalikov@idsgroup.uz>
2019-04-21 17:24:27 +0500

    [+] Добавил основную страницу

---
+++ inplay/resources/views/main/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/fullpage.css" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mainPage.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<div id="header">        
+    <nav class="navbar navbar-dark bg-dark">
+        InPlay
+        <button type="button" class="btn btn-success float-right">Войти</button>
+    </nav>
+</div>
+
+<div id="fullpage">
+    <div class="section" id="section0"></div>
+    <div class="section" id="section1">
+        <div class="slide" id="slide1">
+            <section>
+                <div class="container">
+                  <div class="card">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="card-body">
+                          <h4 class="card-title">Curabitur gravida vestibulum imperdiet.</h4>
+                          <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem. Integer sed mi quis nisl eleifend interdum.</p>
+                          <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem.</p>
+                          <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="card-img-bottom">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </section>
+        </div>
+        <div class="slide" id="slide2">
+            <section>
+                <div class="container">
+                  <div class="card">
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="card-body">
+                          <h4 class="card-title">Curabitur gravida vestibulum imperdiet.</h4>
+                          <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem. Integer sed mi quis nisl eleifend interdum.</p>
+                          <p class="card-text">Cras convallis ut turpis vitae facilisis. Morbi eu augue vel quam efficitur rhoncus vitae eget lectus. Cras augue ligula, aliquam ut enim ut, feugiat imperdiet sem.</p>
+                          <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                       </div>
+                       <div class="col-md-6">
+                         <div class="card-img-bottom">
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <div class="section" id="section2"></div>
+</div>
+
+<script type="text/javascript" src="js/fullpage.js"></script>
+<script type="text/javascript" src="js/fullpageScript.js"></script>
+</body>
+</html>
